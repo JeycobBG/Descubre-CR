8d12710fdf60b9acb54f5c7d14f382d2363a57f6
kvene <kvene@DellKev>
2024-06-01 16:07:27 -0600

    Prueba para UsuarioRuta

--- src/main/java/cr/ac/una/DescubreCR/MvcConfig.java
+++ src/main/java/cr/ac/una/DescubreCR/MvcConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
+/**aaaaaaaaaaaaaaa
  *
  * @author josue
  */
