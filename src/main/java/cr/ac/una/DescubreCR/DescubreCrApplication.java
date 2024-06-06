49435400aaab2e3dcb7b4b2dcda05d4f6b25c500
kvene <kvene@DellKev>
2024-06-06 11:29:45 -0600

    Probando

--- src/main/java/cr/ac/una/DescubreCR/DescubreCrApplication.java
+++ src/main/java/cr/ac/una/DescubreCR/DescubreCrApplication.java
@@ -3,7 +3,7 @@ package cr.ac.una.DescubreCR;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication////////////Revisando
 public class DescubreCrApplication {
 
 	public static void main(String[] args) {
